84658921c8d74e3efcd008dafe89fcfc518d6ce9
Brent Pedersen <bpederse@gmail.com>
2012-12-16 09:31:20 -0700

    separate out some logic for mirroring

--- cruzdb/mirror.py
+++ cruzdb/mirror.py
@@ -1,9 +1,10 @@
 # from http://www.tylerlesmann.com/2009/apr/27/copying-databases-across-platforms-sqlalchemy/
 
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import create_engine, Table, select, Enum, Column, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import LONGBLOB, ENUM
+from sqlalchemy.dialects.postgresql import ENUM as PG_ENUM
 from sqlalchemy.types import VARCHAR
 import sqlalchemy
 import sys
@@ -20,12 +21,6 @@ def make_session(connection_string):
     engine.connect()
     return Session(), engine
 
-def quick_mapper(table):
-    Base = declarative_base()
-    class GenericMapper(Base):
-        __table__ = table
-    return GenericMapper
-
 def page_query(q, limit=70000):
     offset = 0
     while True:
@@ -36,35 +31,59 @@ def page_query(q, limit=70000):
         if elem is None:
             break
 
+def set_table(genome, table, table_name, connection_string, metadata):
+    """
+    alter the table to work between different
+    dialects
+    """
+    table = Table(table_name, genome.Base.metadata, autoload=True,
+                    autoload_with=genome.engine, extend_existing=True)
+
+    print "\t".join([c.name for c in table.columns])
+    # need to prefix the indexes with the table name to avoid collisions
+    for i, idx in enumerate(table.indexes):
+        idx.name = table_name + "." + idx.name + "_ix" + str(i)
+
+    cols = []
+    for i, col in enumerate(table.columns):
+        # convert mysql-specific types to varchar
+        print col.name, col.type, isinstance(col.type, ENUM)
+        if isinstance(col.type, (LONGBLOB, ENUM)):
+
+            if 'sqlite' in connection_string:
+                col.type = VARCHAR()
+            elif 'postgres' in connection_string:
+                if isinstance(col.type, ENUM):
+                    print dir(col)
+                    col.type = PG_ENUM(*col.type.enums, name=col.name,
+                        create_type=True)
+                else:
+                    col.type = VARCHAR()
+        elif str(col.type) == "VARCHAR" \
+                and ("mysql" in connection_string \
+                or "postgres" in connection_string):
+            if col.type.length is None:
+                col.type.length = 48 if col.name != "description" else None
+        if not "mysql" in connection_string:
+            if str(col.type).lower().startswith("set("):
+                col.type = VARCHAR(15)
+        cols.append(col)
+    table = Table(table_name, genome.Base.metadata, *cols,
+            autoload_replace=True, extend_existing=True)
+
+    return table
+
 def mirror(genome, tables, connection_string):
-    #source, sengine = make_session(from_db)
-    #smeta = MetaData(bind=sengine)
     destination, dengine = make_session(connection_string)
+    dmeta = MetaData(bind=dengine)
 
     for table_name in tables:
         # cause it ot be mapped
         table = genome.table(table_name)
-        print 'Processing', table_name
-        table = Table(table_name, genome.Base.metadata, autoload=True,
-                autoload_with=genome.engine)
+        print >>sys.stderr, 'Processing', table_name
 
-        # need to prefix the indexes with the table name to avoid collisions
-        for idx in table.indexes:
-            idx.name = table_name + "." + idx.name
-
-        for col in table.columns:
-            # convert mysql-specific types to varchar
-            if isinstance(col.type, (LONGBLOB, ENUM)) \
-                    and not 'mysql' in connection_string:
-                col.type = VARCHAR()
-            elif str(col.type) == "VARCHAR" \
-                    and "mysql" in connection_string:
-                if col.type.length is None:
-                    col.type.length = 95
-            if not "mysql" in connection_string:
-                if str(col.type).lower().startswith("set("):
-                    col.type = VARCHAR(15)
-
+        table = set_table(genome, table, table_name,
+                connection_string, dmeta)
         try:
             table.create(dengine)
         except sqlalchemy.exc.OperationalError:
