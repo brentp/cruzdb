5ba9a49fb72c7f0a93fbca7f3a2511ae41aaddf8
Brent Pedersen <bpederse@gmail.com>
2013-02-20 18:18:50 -0700

    return gene if other interval contains the entire gene body

--- cruzdb/models.py
+++ cruzdb/models.py
@@ -211,10 +211,13 @@ class ABase(object):
         return e.g. "intron;exon" if the other_start, end overlap introns and
         exons
         """
+        # completely encases gene.
+        if other_start <= self.start and other_end >= self.end:
+            return ['gene']
         other = Interval(other_start, other_end)
         ovls = []
         tx = 'txEnd' if self.strand == "-" else 'txStart'
-        if hasattr(self, tx) and other_start <= self.txStart <= other_end and self.txStart != self.txEnd:
+        if hasattr(self, tx) and other_start <= getattr(self, tx) <= other_end and self.txStart != self.txEnd:
                 ovls = ["TSS"]
         # TODO check txStart == txEnd and return non-coding?
         for ftype in ('introns', 'exons', 'utr5', 'utr3', 'cdss'):
