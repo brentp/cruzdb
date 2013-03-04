df3c4740290c30e9e26e325e2053428674ffe608
Brent Pedersen <bpederse@gmail.com>
2013-03-03 19:55:42 -0700

    update session stuff

--- cruzdb/init.py
+++ cruzdb/init.py
@@ -2,11 +2,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
+#Session = scoped_session(sessionmaker()) #bind=engine))
+Session = sessionmaker(autoflush=False, expire_on_commit=False)
 
 def initialize_sql(engine):
-    Session = sessionmaker(autoflush=False)
-    #if Session.registry.has(): return Session
-    #Session = scoped_session(sessionmaker(bind=engine))
+    global Session
+    #Session = sessionmaker(autoflush=True)
     session = Session(bind=engine)
     Base = declarative_base()
     Base.metadata.bind = engine
