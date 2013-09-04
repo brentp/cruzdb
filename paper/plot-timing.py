9b32a2f0c71f3ec9622917457bcf2659fc06fdc6
Brent Pedersen <bpederse@gmail.com>
2013-09-04 11:54:23 -0600

    use gray-scale in plot

--- paper/plot-timing.py
+++ paper/plot-timing.py
@@ -20,9 +20,11 @@ ax.set_ylabel('Intervals / Second')
 x = 0.1 + np.arange(3)
 width = 0.35
 
-rects0 = ax.bar(x, 3327. / pt.time, width, color='r', edgecolor='r', alpha=0.91)
+
+rects0 = ax.bar(x, 3327. / pt.time, width, color='0.70', alpha=0.91)
+
 rects1 = ax.bar(x[:len(pf.time)] + width + 0.03, 3327. / pf.time, width,
-        color='b', edgecolor='b', alpha=0.91)
+        color='0.3', alpha=0.91)
 
 ax.set_xticks(x + width)
 ax.set_ylim(0, (3327. / min(pf.time)) + 50)
